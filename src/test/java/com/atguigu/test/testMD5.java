06df6ed0153f632a8c5f820f0aa31524e308b9c5
chen <chen.chen123.com>
2022-07-31 00:31:27 +0800

    添加了hotfix 8888

--- src/test/java/com/atguigu/test/testMD5.java
+++ src/test/java/com/atguigu/test/testMD5.java
@@ -9,7 +9,7 @@ public class testMD5 {
     public void testmd55(){
         System.out.println(MD5Utils.md5("123xiao"));
         System.out.println(MD5Utils.md5("123xiaowang"));
-
+        System.out.println("hotfix 8888");
         System.out.println(MD5Utils.md5("f439778d1a302657229cda5e3f286bc6"));
     }
 }
