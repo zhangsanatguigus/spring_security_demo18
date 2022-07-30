d0017a023c3df1553df36a4f822ae03c52597fc8
chen <chen.chen123.com>
2022-07-31 00:30:00 +0800

    添加了master 6666

--- src/test/java/com/atguigu/test/testMD5.java
+++ src/test/java/com/atguigu/test/testMD5.java
@@ -9,7 +9,7 @@ public class testMD5 {
     public void testmd55(){
         System.out.println(MD5Utils.md5("123xiao"));
         System.out.println(MD5Utils.md5("123xiaowang"));
-        System.out.println("master 6666");
+        System.out.println("master 66666");
         System.out.println(MD5Utils.md5("f439778d1a302657229cda5e3f286bc6"));
     }
 }
