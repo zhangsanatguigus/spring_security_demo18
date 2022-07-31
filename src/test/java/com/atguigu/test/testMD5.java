48fcc3b2785f9e71324e4861f4833f3a2cd7036d
zhangsanatguigus <2464511899@qq.com>
2022-07-31 11:14:42 +0800

    添加了master 188

--- src/test/java/com/atguigu/test/testMD5.java
+++ src/test/java/com/atguigu/test/testMD5.java
@@ -11,6 +11,7 @@ public class testMD5 {
         System.out.println(MD5Utils.md5("123xiaowang"));
         System.out.println("master 66666");
         System.out.println("hotfix 8888");
+        System.out.println("master 188");
         System.out.println(MD5Utils.md5("f439778d1a302657229cda5e3f286bc6"));
     }
 }
